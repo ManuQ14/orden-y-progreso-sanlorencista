f6a42d1e3cba280ea97a283e5bee91231ac96608
ManuQ14 <manuquintanafotos@gmail.com>
2026-04-13 00:13:17 -0300

    fix: se suspendio hasta no tener el confirm de descripciones de candidatos el clik de las cards y se re-trabajo la ingeniería de fotos

--- src/views/Candidatos/components/Main.jsx
+++ src/views/Candidatos/components/Main.jsx
@@ -13,135 +13,135 @@ const candidatesData = [
   {
     id: 2,
     name: "Juan Manuel Campos",
-    role: "Vicepresidente 1°",
+    role: "Vicepresidente",
     fullDescription: "Con experiencia empresarial en el rubro de la construcción y el acero, Juan Manuel es Licenciado en X y miembro fundador de Todo x San Lorenzo, una agrupación joven creada en 2024.",
     photo: "/images/candidatos/2.jpg"
   },
   {
     id: 3,
     name: "Juan Bautista Castagna",
-    role: "Vocal Titular N°1",
+    role: "Vocal  N°1",
     fullDescription: "Abogado con experiencia en el sector privado y miembro fundador de Orden y Progreso. Apoderado Legal de la agrupación desde 2022.",
     photo: "/images/candidatos/3.jpg"
   },
   {
     id: 4,
     name: "Aríel Dean",
-    role: "Vocal Titular N°2",
+    role: "Vocal  N°2",
     fullDescription: "Licenciado en Ciencias Políticas. Miembro de la Comisión Fiscalizadora del club y fundador de la agrupación Actitud San Lorenzo.",
     photo: "/images/candidatos/4.jpg"
   },
   {
     id: 5,
     name: "Ana Bozzano",
-    role: "Vocal Titular N°3",
+    role: "Vocal  N°3",
     fullDescription: "Docente",
     photo: "/images/candidatos/5.jpg"
   },
   {
     id: 6,
     name: "Hernán Etman",
-    role: "Vocal Titular N°4",
+    role: "Vocal  N°4",
     fullDescription: "Abogado y ex vicepresidente del CASLA. Fue parte de la reestructuración del club tras la acefalía de 2012, finalizando su mandato en 2013.",
     photo: "/images/candidatos/6.jpg"
   },
   {
     id: 7,
     name: "Gonzalo Campos",
-    role: "Vocal Titular N°5",
+    role: "Vocal  N°5",
     fullDescription: "Profesional con más de 20 años en Fintech. Lic. en Administración (UBA), Maestría en Finanzas (CEMA) y Fintech (ITBA).",
     photo: "/images/candidatos/7.jpg"
   },
   {
     id: 8,
     name: "Pedro Criscolo",
-    role: "Vocal Titular N°6",
+    role: "Vocal  N°6",
     fullDescription: "Fundador de la agrupación Lorenzo Vive e impulsor del tratamiento de la expulsión de Marcelo Moretti a través del Tribunal de Ética.",
     photo: "/images/candidatos/8.jpg"
   },
   {
     id: 9,
     name: "Oscar Alejandro Moreira",
-    role: "Vocal Titular N°7",
+    role: "Vocal  N°7",
     fullDescription: "",
     photo: "/images/candidatos/9.jpg"
   },
   {
     id: 10,
     name: "Claudia Da Costa Díaz",
-    role: "Vocal Titular N°8",
+    role: "Vocal  N°8",
     fullDescription: "Escritora y directora de medios audiovisuales.",
     photo: "/images/candidatos/10.jpg"
   },
   {
     id: 11,
     name: "Daniel Rotella",
-    role: "Vocal Titular N°9",
+    role: "Vocal  N°9",
     fullDescription: "Contador.",
     photo: "/images/candidatos/11.jpg"
   },
   {
     id: 12,
     name: "Maximiliano Ferreira",
-    role: "Vocal Titular N°10",
+    role: "Vocal  N°10",
     fullDescription: "Abogado, cofundador del estudio T&F. Miembro de Todo x San Lorenzo.",
     photo: "/images/candidatos/12.jpg"
   },
   {
     id: 13,
     name: "Silvana Maidana",
-    role: "Vocal Titular N°11",
+    role: "Vocal  N°11",
     fullDescription: "",
     photo: "/images/candidatos/13.jpg"
   },
   {
     id: 14,
     name: "Lucas Rajoy",
-    role: "Vocal Titular N°12",
+    role: "Vocal  N°12",
     fullDescription: "",
     photo: "/images/candidatos/14.jpg"
   },
   {
     id: 15,
     name: "Guillermo Mangone",
-    role: "Vocal Titular N°13",
+    role: "Vocal  N°13",
     fullDescription: "Secretario General de la Federación de Trabajadores de la Industria del Gas Natural de la República Argentina (FETIGNRA).",
     photo: "/images/candidatos/15.jpg"
   },
   {
     id: 16,
     name: "Tomás González Cordásco",
-    role: "Vocal Titular N°14",
+    role: "Vocal  N°14",
     fullDescription: "Parte de la Dirección de Seguridad del Senado de la Nación. Miembro de Actitud San Lorenzo.",
     photo: "/images/candidatos/16.jpg"
   },
   {
     id: 17,
     name: "Diego Espinosa Godoy",
-    role: "Vocal Titular N°15",
+    role: "Vocal  N°15",
     fullDescription: "Abogado y docente universitario, impulsó judicialmente, junto con Marcelo Culotta, la convocatoria a reunión de CD para declarar la acefalía.",
     photo: "/images/candidatos/17.jpg"
   },
   {
     id: 18,
     name: "Mariano Achile",
-    role: "Vocal Titular N°16",
+    role: "Vocal  N°16",
     fullDescription: "",
-    photo: "/images/candidatos/16.jpg"
+    photo: "/images/candidatos/18.jpg"
   },
   {
     id: 19,
     name: "Mariana Vilaseco",
-    role: "Vocal Titular N°17",
+    role: "Vocal  N°17",
     fullDescription: "",
-    photo: "/images/candidatos/17.jpg"
+    photo: "/images/candidatos/19.jpg"
   },
   {
     id: 20,
     name: "Gustavo Fernández Quiroz",
-    role: "Vocal Titular N°18",
+    role: "Vocal  N°18",
     fullDescription: "Contador",
-    photo: "/images/candidatos/18.jpg"
+    photo: "/images/candidatos/20.jpg"
   }
 ];
 
@@ -172,8 +172,9 @@ export const Main = () => {
   }, []);
 
   const openModal = (candidate) => {
-    setSelectedCandidate(candidate);
-    document.body.style.overflow = "hidden";
+    // FUNCIÓN DESACTIVADA TEMPORALMENTE - Descomentar cuando esté la info completa
+    // setSelectedCandidate(candidate);
+    // document.body.style.overflow = "hidden";
   };
 
   const closeModal = () => {
